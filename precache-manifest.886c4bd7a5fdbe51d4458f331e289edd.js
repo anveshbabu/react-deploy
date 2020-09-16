6ceb1320d574bf197433ba31d274b8211ce26f16
anvesh balaji <anvesh.b@globeshout.com>
2020-09-16 18:01:21 +0530

    Updates

---
+++ precache-manifest.886c4bd7a5fdbe51d4458f331e289edd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc93e3ef2071648569fb21c054c49206",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "43bab8d34fa168b97ee5",
+    "url": "/react-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c6f7947aa9d8f7040ca1",
+    "url": "/react-deploy/static/js/2.42518f21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.42518f21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43bab8d34fa168b97ee5",
+    "url": "/react-deploy/static/js/main.8b45c202.chunk.js"
+  },
+  {
+    "revision": "fd79aa3b760d86374cd1",
+    "url": "/react-deploy/static/js/runtime-main.cd498a81.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
